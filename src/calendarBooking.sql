4053d9fde4cb9988e427d15a9c39a5cd05c25fcf
pp-nb <prakhar.pandey@nobroker.in>
2024-10-17 15:40:05 +0530

    Added the docker composition

---
+++ src/calendarBooking.sql
@@ -0,0 +1,54 @@
+CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS `calendarbooking`;
+USE `calendarbooking`;
+
+CREATE TABLE IF NOT EXISTS users (
+   id VARCHAR(255) PRIMARY KEY,
+   username VARCHAR(50) NOT NULL,
+   password CHAR(80) NOT NULL,
+   first_name VARCHAR(50),
+   last_name VARCHAR(50),
+   email VARCHAR(50),
+   mobile VARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `time_slots` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `start_time` DATETIME NOT NULL,
+    `end_time` DATETIME NOT NULL,
+    `is_booked` BOOLEAN DEFAULT FALSE,
+    `owner_id` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `appointments` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `time_slot_id` VARCHAR(255) NOT NULL,
+    `owner_id` VARCHAR(255) NOT NULL,
+    `status` VARCHAR(20) NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `canceled_at` DATETIME NULL,
+    FOREIGN KEY (`time_slot_id`) REFERENCES `time_slots`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `appointment_invitees` (
+    `appointment_id` VARCHAR(255) NOT NULL,
+    `invitee_id` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`appointment_id`, `invitee_id`),
+    FOREIGN KEY (`appointment_id`) REFERENCES `appointments`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`invitee_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `availability` (
+    `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+    `owner_id` VARCHAR(255) NOT NULL,
+    `day_of_week` ENUM('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY') NOT NULL,
+    `start_time` TIME NOT NULL,
+    `end_time` TIME NOT NULL,
+    FOREIGN KEY (`owner_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
